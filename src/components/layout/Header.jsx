7053727905708864e91f52a3b4a57b505e78119a
Jun Seong <drawbeat0112@gmail.com>
2024-09-10 18:01:35 +0900

    feat : header 컴포넌트

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,7 +1,17 @@
+import { Button } from "antd";
+import LOGO from "../../assets/images/Logo.svg";
+import style from "./Header.module.css";
+import { Link } from "react-router-dom";
+
 export default function Header() {
   return (
-    <header>
-      <h2>헤더입니다.</h2>
+    <header className={style.header}>
+      <Link to="/">
+        <img src={LOGO} alt="logo" />
+      </Link>
+      <Button type="primary" size="large">
+        로그인 하기
+      </Button>
     </header>
   );
 }
