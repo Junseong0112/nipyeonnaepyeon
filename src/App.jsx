ae984fe92f554706699a1c18c03de3b345366502
Jun Seong <drawbeat0112@gmail.com>
2024-09-10 20:40:04 +0900

    feat : ant design theme custom

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
+import { ConfigProvider } from "antd";
 import AppRouter from "./router/AppRouter";
 
 function App() {
   return (
-    <>
+    <ConfigProvider
+      theme={{
+        token: {
+          colorPrimary: "#F24E1E",
+          fontFamily: "NixGon",
+        },
+      }}
+    >
       <AppRouter />
-    </>
+    </ConfigProvider>
   );
 }
 
